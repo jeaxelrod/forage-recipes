f50b0a1bd0884fb95b24ac219cddff40a6d7538c
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-04-08 14:14:16 -0700

    Update Karma config to load correct files

--- karma.conf.js
+++ karma.conf.js
@@ -5,10 +5,14 @@ module.exports = function(config){
 
     files : [
       'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'app/bower_components/lodash/lodash.js',
+      'app/bower_components/restangular/dist/restangular.min.js',
+      'app/app.js',
+      'app/services/**/*.js',
+      'app/controllers/**/*.js',
+      'tests/**/*.js'
     ],
 
     autoWatch : true,
